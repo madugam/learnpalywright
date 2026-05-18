57b46e777a8b446b076c932fa412139955a298a2
madugam <madugam@example.com>
2026-05-18 07:40:43 +0530

    Add Chapter 5: JavaScript ternary operator with 2 numbers

---
+++ chapter_05_Javascript/01_ternary2numbers.js
@@ -0,0 +1,9 @@
+// 01_ternary2numbers.js - JavaScript ternary operator example
+let num1 = 10;
+let num2 = 20;
+console.log(num1 > num2 ? "num1 is greater" : "num2 is greater");
+
+let a = 5;
+let b = 10;
+let c = 30;
+console.log(a > b ? "a bigger" : b > c ? "b bigger" : "c bigger");
